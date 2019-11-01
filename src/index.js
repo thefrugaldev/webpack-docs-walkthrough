88775799d82944dfd2f6eebb3f89c461f61796f8
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2019-10-31 22:34:20 -0400

    Completed section 4 in setting up the Development Environment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
   const btn = document.createElement("button");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "from", "webpack"], " ");
 
   btn.innerHTML = "Click me and check the console!";
   btn.onclick = printMe;
